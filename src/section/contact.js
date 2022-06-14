2f355887fd675f08c770568f9b7c5b5354afdb2f
marianialessandro <mariani.alessandro.dev@gmail.com>
2022-06-14 22:21:03 +0200

    Social Icons update

--- src/section/contact.js
+++ src/section/contact.js
@@ -10,10 +10,10 @@ function Contacts() {
                 <p>If you want to get in touch with me you can send me an email or a message on twitter or instagram, i'll try my best to get back to you.</p>
 
                 <div className="iconContainer">
-                    <a href="https://twitter.com/Alessan42045415" className="twitter"><i class="fa fa-twitter"></i></a> 
-                    <a href="https://www.instagram.com/_alessandromariani.03_/" className="instagram"><i class="fa fa-instagram"></i></a>
-                    <a href="https://www.linkedin.com/in/alessandro-mariani-7a53981aa/" className="linkedin"><i class="fa fa-linkedin"></i></a>
-                    <a href="mailto:alessandromariani@marianialessandro.com" className="envelope"><i class="fa fa-envelope"></i></a>
+                    <a href="https://twitter.com/Alessan42045415" className="twitter"><i className="fa fa-twitter"></i></a> 
+                    <a href="https://www.instagram.com/_alessandromariani.03_/" className="instagram"><i className="fa fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/alessandro-mariani-7a53981aa/" className="linkedin"><i className="fa fa-linkedin"></i></a>
+                    <a href="mailto:business@marianialessandro.com" className="envelope"><i className="fa fa-envelope"></i></a>
                 </div>
             </div>
         </div>
--- src/section/presentation.js
+++ src/section/presentation.js
@@ -24,7 +24,7 @@ function Presentation() {
                         <p>I am a student of high school student in the computer science.<br></br>I create applications, scripts, tools of any kind and more.</p>
                     </div>
                     <div className="socialBottomBar">
-                        <a href="https://github.com/alessandro1234-tech"><i className="fa fa-github"></i></a>
+                        <a href="https://github.com/marianialessandro"><i className="fa fa-github"></i></a>
                         <a href="https://www.linkedin.com/in/alessandro-mariani-7a53981aa/"><i className="fa fa-linkedin"></i></a>
                     </div>
                 </div>
